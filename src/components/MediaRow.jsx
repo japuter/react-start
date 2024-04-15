a31cdd423af718ebdb3ad4c0228bd1f97bd58d52
Japuter <jasper.kaira@gmail.com>
2024-04-15 13:01:16 +0300

    aa

---
+++ src/components/MediaRow.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+// src/components/MediaRow.jsx
+
+const MediaRow = (props) => {
+  const {item} = props;
+  return (
+    <tr>
+    <td>
+      <img src={item.thumbnail} alt={item.title} />
+    </td>
+    <td>{item.title}</td>
+    <td>{item.description}</td>
+    <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+    <td>{item.filesize}</td>
+    <td>{item.media_type}</td>
+  </tr>
+  );
+};
+
+MediaRow.propTypes = {
+  item: PropTypes.object.isRequired,
+};
+
+export default MediaRow;
